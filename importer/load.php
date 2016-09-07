6b98b684bc088573299a28594a46b7a5d63e64bf
gogoit1 <ryan@gogoit.com>
2016-09-07 12:04:45 -0700

    Add files via upload

--- importer/load.php
+++ importer/load.php
@@ -3,12 +3,12 @@
 class Tesseract_Importer_Constants {
 	public static $IMPORTED_PACKAGES_OPTION_NAME = 'tesseract_imported_packages';
 	public static $IMPORTED_BY_PACKAGE_META_KEY = '_imported_by_tesseract_package';
-	public static $TRACKED_POST_TYPES = array('fl-builder-template');
+	public static $TRACKED_POST_TYPES = array( 'fl-builder-template' );
 	public static $CONTENT_BLOCK_META_KEY = '_imported_content_block';
 }
 
-if ( ! defined('TESSERACT_PACKAGES_FILE')) {
-	define('TESSERACT_PACKAGES_FILE', dirname(__FILE__).'/data/packages.json');
+if ( ! defined( 'TESSERACT_PACKAGES_FILE' ) ) {
+	define( 'TESSERACT_PACKAGES_FILE', dirname( __FILE__ ) . '/data/packages.json' );
 }
 
 require 'required-plugins.php';
